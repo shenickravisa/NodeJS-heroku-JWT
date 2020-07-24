0146e5b26af13d9194208e0745830d33a89df3df
shenickravisa <cualquiercosa>
2020-07-24 18:56:40 -0500

    SG: save current work

--- app.js
+++ app.js
@@ -10,12 +10,15 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import path from 'path';
-const history = require('connect-history-api-fallback')
 const app = express()
+
+/**
+ * Middleware
+ */
 app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.json())
-
+app.use(express.urlencoded({ extended: true }))
 /**
  * creacion de ruta principal
  */
@@ -25,8 +28,9 @@ app.get('/', function (req, res) {
 /**
  * middleware para vue 
  */
+const history = require('connect-history-api-fallback')
 app.use(history())
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 /**
  * Levantar puerto del servidor
